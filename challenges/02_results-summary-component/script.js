c0e0d938477a07895a711636ca62494b2f7f272c
bkukhalashvili <bacho.kukhalashvili.1@gmail.com>
2024-05-08 20:27:59 +0400

    added js code to fetch data from data.json and to generate html

---
+++ challenges/02_results-summary-component/script.js
@@ -0,0 +1,38 @@
+const displayData = function () {
+  let componentSkills = document.querySelector(".component__right__skills");
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((json) => {
+      const info = json;
+      info.forEach((obj) => {
+        componentSkills.innerHTML += `
+        <div class="component__right__skills__container">
+        <div
+          class="component__right__skills__container__text-box ${obj.category.toLowerCase()}"
+        >
+          <img src=${obj.icon} alt="" />
+          <span
+            class="component__right__skills__container__text-box__text"
+          >
+          ${obj.category}</span
+          >
+        </div>
+        <div class="component__right__skills__container__score">
+          <span
+            class="component__right__skills__container__score__number"
+          >
+            ${obj.score}
+          </span>
+          <span
+            class="component__right__skills__container__score__out-of"
+          >
+            / 100
+          </span>
+        </div>
+      </div>
+        `;
+      });
+    });
+};
+
+displayData();
